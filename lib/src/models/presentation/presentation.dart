c3979a43a1ddcd5657cdcf86472db959732726a5
Rerurate514 <rerurateyuto@gmail.com>
2025-08-13 17:43:22 +0900

    notifiersプロパティを追加

--- lib/src/models/presentation/presentation.dart
+++ lib/src/models/presentation/presentation.dart
@@ -1,3 +1,4 @@
+import 'package:clean_arch_gen/src/models/presentation/notifier.dart';
 import 'package:clean_arch_gen/src/models/presentation/page.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 
@@ -7,7 +8,8 @@ part 'presentation.g.dart';
 @freezed
 sealed class Presentation with _$Presentation {
   const factory Presentation({
-    required List<Page> pages
+    required List<Page> pages,
+    required List<Notifier> notifiers
   }) = _Presentation;
 
   factory Presentation.fromJson(Map<String, dynamic> json) => _$PresentationFromJson(json);
