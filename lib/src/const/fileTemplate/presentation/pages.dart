2f70203ac2c8fcce8eed073dc1be570cb70a17eb
Rerurate514 <rerurateyuto@gmail.com>
2025-08-13 14:33:58 +0900

    presentationテンプレート生成

---
+++ lib/src/const/fileTemplate/presentation/pages.dart
@@ -0,0 +1,20 @@
+import 'package:clean_arch_gen/src/models/presentation/page.dart';
+
+String createPages(Page page){
+  return """
+import 'package:flutter/material.dart';
+
+class ${page.name}Page extends StatelessWidget {
+  const ${page.name}Page({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: SafeArea(
+        child: Container(),
+    );
+  }
+}
+
+""";
+}
