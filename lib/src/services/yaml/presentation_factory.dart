9785f78e23c1e54c8d3a73e7c1031747911329a0
Rerurate514 <rerurateyuto@gmail.com>
2025-08-15 04:03:47 +0900

    presentation_factoryを作成

---
+++ lib/src/services/yaml/presentation_factory.dart
@@ -0,0 +1,40 @@
+import 'package:clean_arch_gen/src/models/presentation/presentation.dart';
+import 'package:clean_arch_gen/src/models/presentation/page.dart';
+import 'package:clean_arch_gen/src/models/presentation/notifier.dart';
+import 'package:clean_arch_gen/src/utils/paths.dart';
+import 'package:yaml/yaml.dart';
+
+class PresentationFactory {
+  Presentation createPresentation(YamlMap yaml) {
+    return Presentation(
+      pages: _createPages(yaml), 
+      notifiers: _createNotifiers(yaml)
+    );
+  }
+
+  List<Page> _createPages(YamlMap yaml) {
+    final presentationYaml = yaml[Paths.presentation.path];
+    if (presentationYaml == null || presentationYaml[Paths.presentation.path] == null) {
+      return [];
+    }
+    
+    final pagesYaml = presentationYaml[Paths.presentation.path] as YamlList;
+    return pagesYaml
+        .cast<String>()
+        .map((pageName) => Page(name: pageName))
+        .toList();
+  }
+
+  List<Notifier> _createNotifiers(YamlMap yaml) {
+    final presentationYaml = yaml[Paths.presentation.path];
+    if (presentationYaml == null || presentationYaml[Paths.presentationNotifiers.path] == null) {
+      return [];
+    }
+    
+    final notifiersYaml = presentationYaml[Paths.presentationNotifiers.path] as YamlList;
+    return notifiersYaml
+        .cast<String>()
+        .map((notifierName) => Notifier(name: notifierName))
+        .toList();
+  }
+}
