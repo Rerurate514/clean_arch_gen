d45e943f71630f0914616db831b829b46d7224ed
Rerurate514 <rerurateyuto@gmail.com>
2025-08-16 22:45:19 +0900

    コマンドを実行できるように修正

--- bin/clean_arch_gen.dart
+++ bin/clean_arch_gen.dart
@@ -1,3 +1,7 @@
+#!/usr/bin/env dart
+
+// ignore: unused_import
+import 'package:clean_arch_gen/clean_arch_gen.dart';
 import 'package:clean_arch_gen/src/services/command/command_manager.dart';
 
 void main(List<String> arguments) async {
