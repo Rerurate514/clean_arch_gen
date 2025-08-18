9de15fadf96c9ab6a5a7ec160e509725a7abf92a
Rerurate514 <rerurateyuto@gmail.com>
2025-08-18 20:49:05 +0900

    フィールドが生成されるように修正

--- lib/src/const/fileTemplate/domain/entity.dart
+++ lib/src/const/fileTemplate/domain/entity.dart
@@ -1,7 +1,11 @@
 import 'package:clean_arch_gen/src/models/domain/entity.dart';
 import 'package:clean_arch_gen/src/utils/recase.dart';
 
-String createEntity(Entity entity){
+String createEntity(Entity entity) {
+  final fields = entity.classFields
+      .map((classField) => "required ${classField.type} ${classField.name},")
+      .join('\n    ');
+
   return """
 import 'package:freezed_annotation/freezed_annotation.dart';
 
@@ -11,7 +15,7 @@ part '${entity.name.toLowerCamelCase()}.g.dart';
 @freezed
 sealed class ${entity.name} with _\$${entity.name} {
   const factory ${entity.name}({
-    
+    $fields
   }) = _${entity.name};
 
   factory ${entity.name}.fromJson(Map<String, dynamic> json) => _\$${entity.name}FromJson(json);
