c019289363d8e33d549fd75f7e715517dcc556d5
Rerurate514 <rerurateyuto@gmail.com>
2025-08-21 16:23:42 +0900

    コマンドオプションを追加

--- lib/src/services/command/command_options.dart
+++ lib/src/services/command/command_options.dart
@@ -1,6 +1,12 @@
 import 'dart:io';
+import 'package:clean_arch_gen/src/services/file/application_file_creater.dart';
+import 'package:clean_arch_gen/src/services/file/directory_creater.dart';
+import 'package:clean_arch_gen/src/services/file/domain_file_creater.dart';
+import 'package:clean_arch_gen/src/services/file/file_system_service.dart';
+import 'package:clean_arch_gen/src/services/file/infrastructure_file_creater.dart';
+import 'package:clean_arch_gen/src/services/file/presentation_file_creater.dart';
 import 'package:clean_arch_gen/src/services/yaml/yaml_analyzer.dart';
-import 'package:clean_arch_gen/src/utils/recase.dart';
+import 'package:clean_arch_gen/src/utils/exceptions/file_not_found_exception.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 
 part 'command_options.freezed.dart';
@@ -18,51 +24,92 @@ sealed class CommandOptions with _$CommandOptions {
 }
 
 extension CommandOptionsEx on CommandOptions {
-  String parse() {
+  (String, String?, bool) parse() {
+    String? outputFilePath;
+    bool forceOverwrite = false;
+
+    List<String> remainingArgs = [];
     for (int i = 0; i < args.length; i++) {
       String arg = args[i];
       switch (arg) {
         case '-d':
         case '--directory':
           if (i + 1 < args.length) {
-            return args[i + 1];
+            return (args[i + 1], outputFilePath, forceOverwrite);
           }
+          break;
+        case '-o':
+        case '--output':
+          if (i + 1 < args.length) {
+            outputFilePath = args[i + 1];
+            i++;
+          }
+          break;
+        case '-f':
+        case '--force':
+          forceOverwrite = true;
+          break;
+        default:
+          remainingArgs.add(arg);
           break;
       }
     }
-    return args.isNotEmpty ? args[0] : '';
-  }
 
-  List<String> getExecutionCommand() {
-    String filePath = parse();
-    if (filePath.isEmpty) return [];
-    
-    String extension = filePath.split('.').last.toLowerCamelCase();
-    switch (extension) {
-      case 'yaml':
-      case 'yml':
-        return ['type', filePath];
-      default: return Platform.isWindows ? [filePath] : ['./${filePath.split('/').last}'];
-    }
+    return (remainingArgs.isNotEmpty ? remainingArgs[0] : '', outputFilePath, forceOverwrite);
   }
 
   Future<void> execute() async {
-    String filePath = parse();
+    final (String filePath, String? outputFilePath, bool forceOverwrite) = parse();
+
     if (filePath.isEmpty) {
-      throw Exception('ファイルパスが指定されていません');
+      throw FileNotFoundException(message: 'ファイルパスが指定されていません');
     }
 
     File file = File(filePath);
     if (!await file.exists()) {
-      throw Exception('ファイルが見つかりません: $filePath');
+      throw FileNotFoundException(message: 'ファイルが見つかりません: $filePath');
     }
 
-    List<String> command = getExecutionCommand();
-    if (command.isEmpty) {
-      throw Exception('実行コマンドを生成できません');
+    if (outputFilePath != null) {
+      final outputDirectory = Directory(outputFilePath);
+      if (forceOverwrite && await outputDirectory.exists()) {
+        print('既存のディレクトリを削除しています: ${outputDirectory.path}');
+        await outputDirectory.delete(recursive: true);
+      }
+
+      if (!forceOverwrite && await outputDirectory.exists()) {
+        throw Exception('出力ディレクトリが既に存在します。上書きするには --force オプションを使用してください: ${outputDirectory.path}');
+      }
     }
 
     final YamlAnalyzer yamlAnalyzer = YamlAnalyzer();
-    yamlAnalyzer.analyze(file);
+    final layersModel = await yamlAnalyzer.analyze(file);
+
+    final FileSystemService fileSystemService = RealFileSystemService();
+
+    final DirectoryCreater directoryCreater = DirectoryCreater(
+      fileSystemService: fileSystemService, 
+      yaml: file
+    );
+
+    await directoryCreater.createProject();
+
+    final applicationFileCreater = ApplicationFileCreater(fileSystemService);
+    final domainFileCreater = DomainFileCreater(fileSystemService);
+    final infrastructureFileCreater = InfrastructureFileCreater(fileSystemService);
+    final presentationFileCreater = PresentationFileCreater(fileSystemService);
+
+    applicationFileCreater.create(file, layersModel.domain);
+    domainFileCreater.create(file, layersModel.domain, layersModel.infrastructure);
+    infrastructureFileCreater.create(file, layersModel.domain, layersModel.infrastructure);
+    presentationFileCreater.create(file, layersModel.domain, layersModel.presentation);
+
+    // TODO
+    // 必要に応じて、yamlAnalyzerの結果をoutputFileに書き込むロジックを追加
+    // 例:
+    // if (outputFile != null) {
+    //   await outputFile.writeAsString(yamlAnalyzer.result.toString());
+    //   print('解析結果を ${outputFile.path} に出力しました。');
+    // }
   }
 }
--- lib/src/services/yaml/yaml_analyzer.dart
+++ lib/src/services/yaml/yaml_analyzer.dart
@@ -1,21 +1,18 @@
 import 'dart:io';
 
-import 'package:clean_arch_gen/src/services/file/application_file_creater.dart';
-import 'package:clean_arch_gen/src/services/file/directory_creater.dart';
-import 'package:clean_arch_gen/src/services/file/domain_file_creater.dart';
-import 'package:clean_arch_gen/src/services/file/file_system_service.dart';
-import 'package:clean_arch_gen/src/services/file/infrastructure_file_creater.dart';
-import 'package:clean_arch_gen/src/services/file/presentation_file_creater.dart';
+import 'package:clean_arch_gen/src/models/utils/Layers_model.dart';
 import 'package:clean_arch_gen/src/services/yaml/domain_factory.dart';
 import 'package:clean_arch_gen/src/services/yaml/infrastructure_factory.dart';
 import 'package:clean_arch_gen/src/services/yaml/presentation_factory.dart';
+import 'package:clean_arch_gen/src/utils/exceptions/file_not_found_exception.dart';
 import 'package:yaml/yaml.dart';
 
 class YamlAnalyzer {
-  void analyze(File file) async {
+  Future<LayersModel> analyze(File file) async {
     if (!await file.exists()) {
-      return;
+      throw FileNotFoundException(message: "${file}が見つからず、yaml解析を実行することができませんでした。");
     }
+
     final yamlString = await file.readAsString();
     final yaml = loadYaml(yamlString) as YamlMap;
 
@@ -27,23 +24,6 @@ class YamlAnalyzer {
     final infrastructure = infrastructureFactory.createInfrastructure(yaml);
     final presentation = presentationFactory.createPresentation(yaml);
 
-    final FileSystemService fileSystemService = RealFileSystemService();
-
-    final DirectoryCreater directoryCreater = DirectoryCreater(
-      fileSystemService: fileSystemService, 
-      yaml: file
-    );
-
-    await directoryCreater.createProject();
-
-    final applicationFileCreater = ApplicationFileCreater(fileSystemService);
-    final domainFileCreater = DomainFileCreater(fileSystemService);
-    final infrastructureFileCreater = InfrastructureFileCreater(fileSystemService);
-    final presentationFileCreater = PresentationFileCreater(fileSystemService);
-
-    applicationFileCreater.create(file, domain);
-    domainFileCreater.create(file, domain, infrastructure);
-    infrastructureFileCreater.create(file, infrastructure, domain);
-    presentationFileCreater.create(file, presentation, domain);
+    return LayersModel(domain: domain, infrastructure: infrastructure, presentation: presentation);
   }
 }
